74992b3aef040f479011826b670c2c5483d5badd
williamedge <williamedge88@gmail.com>
2024-11-12 11:49:50 +0800

    new setup

--- setup.py
+++ setup.py
@@ -1,21 +1,2 @@
-#!/usr/bin/env python
-
-from setuptools import setup, find_packages
-from setuptools.dist import Distribution
-
-class BinaryDistribution(Distribution):
-    def is_pure(self):
-        return False
-
-setup(name='gptide',
-    version='0.3.0',
-    url='https://github.com/TIDE-ITRH/gptide',
-    description='Gaussian Process regression tools for oceanography and engineering',
-    author='Lachlan Astfalck; Matt Rayson; Andrew Zulberti',
-    author_email='lachlan.astfalck@uwa.edu.au',
-    packages=find_packages(),
-    install_requires=['numpy', 'matplotlib', 'scipy', 'xarray','emcee'],
-    license='MIT',
-    include_package_data=True,
-    distclass=BinaryDistribution,
-)
+from setuptools import setup
+setup()
